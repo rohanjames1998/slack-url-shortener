16167814cd9dd5c78a4b1a5827126cad86947a82
Rohan James <rohanjames9282@gmail.com>
2023-07-17 17:16:49 +0530

    Add #create to shortened_urls controller

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -1,2 +1,11 @@
 class ShortenedUrlsController < ApplicationController
+  def create
+    shortened_url = ShortenedUrl.new(full_url: params[:text])
+    shortened_url.add_short_url
+
+    if shortened_url.save
+      SlackClient.web_client.chat_postMessage(channel: params[:channel_name],
+      text: "https://bddd-103-83-145-175.ngrok-free.app/#{shortened_url.short_url}")
+    end
+  end
 end
