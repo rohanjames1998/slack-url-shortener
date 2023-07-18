d2a2f0f9359ae54649016e1d7af843defe78a7bc
Rohan James <rohanjames9282@gmail.com>
2023-07-18 11:10:23 +0530

    Add logic to check if url submitted already exists in db

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -1,10 +1,15 @@
 class ShortenedUrlsController < ApplicationController
+
   def create
+    return if url_found?(params[:text], params[:channel_name])
     short_url = helpers.unique_string
-    shortened_url = ShortenedUrl.find_or_create_by(full_url: params[:text], short_url: short_url)
+    shortened_url = ShortenedUrl.new(full_url: params[:text], short_url: short_url)
     if shortened_url.save
       SlackClient.web_client.chat_postMessage(channel: params[:channel_name],
-      text: "https://bddd-103-83-145-175.ngrok-free.app/#{shortened_url.short_url}")
+      text: "https://3a43-103-83-145-211.ngrok-free.app/#{shortened_url.short_url}")
+    else
+      SlackClient.web_client.chat_postMessage(channel: params[:channel_name],
+      text: "An unexpected error occurred. Please check the url provided.")
     end
   end
 
@@ -17,4 +22,16 @@ class ShortenedUrlsController < ApplicationController
     end
   end
 
+  private
+  def url_found?(full_url, channel_name)
+    if ShortenedUrl.exists?(full_url: full_url)
+      shortened_url = ShortenedUrl.find_by(full_url: full_url)
+      SlackClient.web_client.chat_postMessage(channel: channel_name,
+      text: "https://3a43-103-83-145-211.ngrok-free.app/#{shortened_url.short_url}")
+      # returning true if the url already exists in the database
+      true
+    else
+      false
+    end
+  end
 end
